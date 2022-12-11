09757c6a97baa8a93603af746b5ba3fc9d114ad5
Luan Lima <luan@yata.dev>
2022-12-11 18:54:06 -0300

    :pencil: Update storybbok Avatar and Box components

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,11 @@ export default {
     src: 'https://github.com/luanyata.png',
     alt: 'Luan Lima',
   },
+  argTypes: {
+    src: {
+      control: { type: 'text' },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -7,6 +7,11 @@ export default {
   args: {
     children: <Text>Testando o componente em tela</Text>,
   },
+  argTypes: {
+    children: {
+      control: { type: null },
+    },
+  },
 } as Meta<BoxProps>
 
 export const Primary: StoryObj<BoxProps> = {}

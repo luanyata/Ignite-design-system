5e67bfec056a18511352570e1f089e9264b42c6f
Luan Lima <luan@yata.dev>
2022-12-11 11:40:33 -0300

    :recycle: Improvements in defining text variants

--- packages/react/src/components/Text.tsx
+++ packages/react/src/components/Text.tsx
@@ -1,5 +1,6 @@
 import { styled } from '../styles'
 import { ComponentProps, ElementType } from 'react'
+import { fontSizes } from '@ignite-ui/tokens'
 
 export const Text = styled('p', {
   fontFamily: '$default',
@@ -8,21 +9,15 @@ export const Text = styled('p', {
   color: '$gray100',
 
   variants: {
-    size: {
-      xxs: { fontSize: '$xxs' },
-      xs: { fontSize: '$xs' },
-      sm: { fontSize: '$sm' },
-      md: { fontSize: '$md' },
-      lg: { fontSize: '$lg' },
-      xl: { fontSize: '$xl' },
-      '2xl': { fontSize: '$2xl' },
-      '4xl': { fontSize: '$4xl' },
-      '5xl': { fontSize: '$5xl' },
-      '6xl': { fontSize: '$6xl' },
-      '7xl': { fontSize: '$7xl' },
-      '8xl': { fontSize: '$8xl' },
-      '9xl': { fontSize: '$9xl' },
-    },
+    size: Object.keys(fontSizes).reduce<Record<string, { fontSize: string }>>(
+      (acc, cur) => {
+        acc[cur] = {
+          fontSize: `$${cur}`,
+        }
+        return acc
+      },
+      {},
+    ),
   },
 
   defaultVariants: {
